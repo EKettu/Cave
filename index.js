270367dff76db695bab58e57c8819462238ea458
EKettu <elyne.kettu@gmail.com>
2020-11-22 15:42:27 +0200

    refactoring

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const path = require('path')
-const shortid = require("shortid")
-const Game = require('./models/game')
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+const shortid = require('shortid');
+const Game = require('./models/game');
 
-app.use(cors())
-app.use(express.json()) 
-app.use(express.static(path.join(__dirname, 'cave-frontend/build')))
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'cave-frontend/build')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/cave-frontend/build/index.html'))
-})
+  res.sendFile(path.join(__dirname + '/cave-frontend/build/index.html'));
+});
 
 app.get('/api/games', (req, res) => {
   Game.find({}).then(games => {
-    res.json(games.map(game => game.toJSON()))
-  })
-})
+    res.json(games.map(game => game.toJSON()));
+  });
+});
 
 app.post('/api/games', (req, res, next) => {
-  const body = req.body
+  const body = req.body;
   const game = new Game({
     id: shortid.generate(),
     date: body.date,
@@ -30,23 +30,23 @@ app.post('/api/games', (req, res, next) => {
     batteries_used: body.batteries_used
   });
   game.save().then(savedGame => {
-    res.json(savedGame)
+    res.json(savedGame);
   })
-  .catch(error => next(error))
-})
+  .catch(error => next(error));
+});
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
   if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
+  next(error);
+};
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
--- models/game.js
+++ models/game.js
@@ -1,37 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
-    console.log('connected to MongoDB')
+    console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    console.log('error connecting to MongoDB:', error.message);
+  });
 
 const gameSchema = new mongoose.Schema({
-    date: { 
-        type: Date,
-        required: true
-    },
-    player_won: { 
-        type: Boolean,
-        requred: true
-    },
-
-    batteries_used: {
-        type: Number,
-        required: true
-    }
-})
+  date: {
+    type: Date,
+    required: true
+  },
+  player_won: {
+    type: Boolean,
+    requred: true
+  },
+  batteries_used: {
+    type: Number,
+    required: true
+  }
+});
 
 gameSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-module.exports = mongoose.model('Game', gameSchema)
+module.exports = mongoose.model('Game', gameSchema);
